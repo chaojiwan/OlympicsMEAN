89b701a4329945f9e8c43f2f45488c1643369842
Jin Liang <jin.liang2040@gmail.com>
2016-05-19 11:32:30 -0400

    Add mongo connection and index logic

---
+++ server/app.js
@@ -0,0 +1,24 @@
+"use strict"
+
+let express = require("express");
+let app = express();
+
+let mongoUtil = require('./mongoUtil');
+mongoUtil.connect();
+
+app.use(express.static(__dirname + "/../client"));
+
+app.get('/sports', (request, response) => {
+    let sports = mongoUtil.sports();
+
+    sports.find().toArray((err, docs) => {
+        if (err) {
+            response.sendStatus(400);
+        }
+        console.log(JSON.stringify(docs));
+        let sportNames = docs.map((sport) => sport.name);
+        response.json(sportNames);
+    });
+});
+
+app.listen(8181, () => console.log("Listening on 8181"));
